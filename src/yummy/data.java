6262c8d774983d2b4f4fd7a193e83b2f707f1916
Amit binu <Amit binu>
2017-04-28 18:33:16 -0400

    deleted some lines and renamed a variable

--- src/yummy/data.java
+++ src/yummy/data.java
@@ -6,16 +6,14 @@ public class data {
 	public Restaurant[] restaurants;
 	
 	public data(Stack<String> foods, String origin, String city_name) throws Exception{
-		GeoCoder data = new GeoCoder(city_name , foods, origin);
-		Restaurant[] info = new Restaurant[data.distance_getter().size()];
-	//	System.out.println(data.distance_getter().size() + " This is the size of info");
+		GeoCoder details = new GeoCoder(city_name , foods, origin);
+		Restaurant[] info = new Restaurant[details.distance_getter().size()];
 		String food = foods.pop();
 		int count =0; //count variable to count the number of nulls in the restaurant_names / distnace_getter() etc. 
 		for (int i = 0; i < info.length; i++){
-			if (data.restaurant_names().get(i) == null){
+			if (details.restaurant_names().get(i) == null){
 				count++;
 				if(i != info.length -1){
-					System.out.println("This is the i value of null" + i);
 					food = foods.pop(); 
 					continue;
 				}
@@ -23,24 +21,30 @@ public class data {
 					break;
 				}
 			}
-			String name  = data.restaurant_names().get(i);
-			String address = data.address().get(i);
-			Double distance = data.distance_getter().get(i);
-			Double time = data.time_getter().get(i);
-			Double rating =data.ratings().get(i);
-			System.out.println("");
-			System.out.println(name + " "  + address + " " + distance + " " + time + " " + rating);
+			String name  = details.restaurant_names().get(i);
+			String address = details.restaurant_addresses().get(i);
+			Double distance = details.distance_getter().get(i);
+			Double time = details.time_getter().get(i);
+			Double rating =details.ratings().get(i);
+			
 			info[i] = new Restaurant(name, address, time, distance, rating, food);
 		}
 		restaurants = new Restaurant[info.length - count];
 		
 		int index = 0;
 		for (int i =0; i < info.length; i ++){
+			try{
 			if (info[i].restuarant_name != null){
+				
 				restaurants[index] = info[i];
 				index++;
+				}
+			}
+				catch(NullPointerException e){
+				continue;
+				}
 			}
 		}
 		
-	}
+	
 }
