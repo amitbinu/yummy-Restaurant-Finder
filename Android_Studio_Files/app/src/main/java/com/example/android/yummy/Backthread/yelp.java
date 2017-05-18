4e8b52bf4a9e576fa007b7fd747032a72d5139c3
Amit binu <binua@mcmaster.ca>
2017-05-17 22:46:29 -0400

    Calls the yelp API to get more info about a restaurant

---
+++ Android_Studio_Files/app/src/main/java/com/example/android/yummy/Backthread/yelp.java
@@ -0,0 +1,98 @@
+package com.example.android.yummy.Backthread;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.example.android.yummy.DataManager.Result;
+import com.yelp.fusion.client.connection.YelpFusionApi;
+import com.yelp.fusion.client.connection.YelpFusionApiFactory;
+import com.yelp.fusion.client.models.Business;
+import com.yelp.fusion.client.models.SearchResponse;
+
+
+public class yelp{
+	private static retrofit2.Call<SearchResponse> call;
+	private static String price = "";
+	private static String phone = "";
+	private static String name = "";
+	private static String address = "";
+	private static ArrayList<Business> busy = new ArrayList<Business>();
+	public yelp(String name, String address) throws IOException{
+
+		this.name = name;
+		this.address = address;
+
+		//	System.out.println((double)System.currentTimeMillis() - start );
+		new hope().execute();
+	}
+
+
+
+	class hope extends AsyncTask<String, Void, String[]>{
+
+		@Override
+		protected String[] doInBackground(String... Params) {
+            Log.e("RUNNING", "Still Running");
+			String[] result = new String[2];
+			retrofit2.Response<SearchResponse> response  = null;
+			try{
+				YelpFusionApiFactory apiFactory = new YelpFusionApiFactory();
+				YelpFusionApi yelpFusioinApi = apiFactory.createAPI("EKQF7reSwACI45yGJiBhlg", "FqRhQWJCvmzontKsjalqqd7eb9EnnNv95TEO3hVGdRfkH3CoMSBlUR0NWHbnAEyi");
+
+				Map<String, String> params = new HashMap<>();
+
+
+				//	Double start = (double) System.currentTimeMillis();
+
+				params.put("term", name);
+				params.put("location", address);
+				params.put("sort_by", "best_match");
+				//params.put("longitude", "-79.716575");
+
+				call = yelpFusioinApi.getBusinessSearch(params);
+				response = call.execute();
+
+				//	busy = business;
+					price = response.body().getBusinesses().get(0).getPrice();
+					phone = response.body().getBusinesses().get(0).getDisplayPhone();
+
+				result[0] = price;
+					result[1] = phone;
+				//	return result;
+			}
+			catch (Exception e){
+				Log.e("ERROR", "HOLY MOTHER OF GOD", e);
+				//return null;
+			}
+			if (response != null){
+				price = response.body().getBusinesses().get(0).getPrice();
+				phone = response.body().getBusinesses().get(0).getDisplayPhone();
+				Log.e("Business", response.body().getBusinesses().toString());
+			}
+
+			return result;
+		}
+
+		@Override
+		protected void onPostExecute(String[] strings) {
+			//Result.result.setText(strings[0] + " " + strings[1]);
+			Result.setter(price);
+			super.onPostExecute(strings);
+		}
+	}
+
+
+
+
+	public String  Prices(){
+		return price;
+	}
+	public String Phone() {return phone;}
+	}
+
+
