a42c0cd98abf6600d8f9d12d0fdb3d69e0ba4c44
Amit binu <binua@mcmaster.ca>
2017-05-19 20:06:42 -0400

    Changed the name of this directory

---
+++ Android_Studio_Files/app/src/main/java/com/example/android/yummy/apiCalls/PopularRestaurants.java
@@ -0,0 +1,54 @@
+package com.example.android.yummy.Backthread;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.example.android.yummy.DataManager.GeoCoder;
+import com.example.android.yummy.MainActivities.MainActivity;
+import com.google.maps.GeoApiContext;
+import com.google.maps.PlacesApi;
+import com.google.maps.model.PlacesSearchResponse;
+
+import java.util.Stack;
+
+import static com.example.android.yummy.Backthread.restaurant_getter.city;
+
+/**
+ * Created by amitb on 2017-05-11.
+ */
+
+public class PopularRestaurants {
+    private static String communityName;
+    private static GeoApiContext context;
+    public PlacesSearchResponse address;
+    private static MainActivity object;
+    public PopularRestaurants(String communityName,GeoApiContext context, MainActivity object){
+        this.address = null;
+        this.communityName = communityName;
+        this.context = context;
+        this.object = object;
+        new geoCoding().execute();
+    }
+    class geoCoding extends AsyncTask<String, Void, PlacesSearchResponse> {
+
+
+        @Override
+        protected PlacesSearchResponse doInBackground(String... params) {
+
+            try{
+                address = PlacesApi.textSearchQuery(context, "Best " + " restaurants in " + communityName).await();
+            }
+            catch (Exception e){
+                Log.e("ERROR IN POPULAR REST", e.getMessage(), e);
+            }
+            return address;
+
+        }
+
+        @Override
+        protected void onPostExecute(PlacesSearchResponse placesSearchResponse) {
+            object.datafetcher();
+            super.onPostExecute(placesSearchResponse);
+        }
+    }
+}
---
+++ Android_Studio_Files/app/src/main/java/com/example/android/yummy/apiCalls/restaurant_getter.java
@@ -0,0 +1,64 @@
+package com.example.android.yummy.Backthread;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.example.android.yummy.DataManager.GeoCoder;
+import com.google.maps.GeoApiContext;
+import com.google.maps.PlacesApi;
+import com.google.maps.model.PlacesSearchResponse;
+
+import java.net.URL;
+import java.util.Stack;
+
+/**
+ * Created by amitb on 2017-05-05.
+ */
+
+public class restaurant_getter {
+    public static String origin, city;
+    public static Stack<String> FOOD;
+    public static GeoApiContext context;
+    public static String food;
+    public PlacesSearchResponse address;
+    public static geoCoding running;
+    private static GeoCoder object;
+
+    public restaurant_getter(String origin, String city, Stack<String> FOOD, GeoApiContext context, String food, GeoCoder test){
+        this.origin = origin;
+        this.city = city;
+        this.FOOD = FOOD;
+        this.context = context;
+        this.food = food;
+        this.object = test;
+        running = new geoCoding();
+        running.execute();
+    }
+
+    class geoCoding extends AsyncTask<String, Void, PlacesSearchResponse> {
+
+        @Override
+        protected PlacesSearchResponse doInBackground(String... params) {
+                try{
+                    address = PlacesApi.textSearchQuery(context, food + " restaurants in " + city).await();
+                }
+                catch (Exception e){
+                    Log.e("ERROR", e.getMessage(), e);
+                }
+                return address;
+
+        }
+
+        @Override
+        protected void onPostExecute(PlacesSearchResponse strings) {
+            //Result.result.setText(strings[0] + " " + strings[1]);
+            try{
+
+            object.afterWAIT();}
+            catch (Exception e){
+                Log.e("ERROR IN doInBACKGROUND", e.getMessage(), e);
+            }
+            super.onPostExecute(strings);
+        }
+    }
+}
